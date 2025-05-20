bb96bf58bd70317b63c3b35f3c878c05d21d6f28
Benno Lossin <lossin@kernel.org>
2025-05-21 01:01:32 +0200

    rust: miscdevice: replace `MaybeUninit::zeroed().assume_init()` with `pin_init::zeroed()`
    All types in `bindings` implement `Zeroable` if they can, so use
    `pin_init::zeroed` instead of relying on `unsafe` code.
    
    If this ends up not compiling in the future, something in bindgen or on
    the C side changed and is most likely incorrect.
    
    Signed-off-by: Benno Lossin <lossin@kernel.org>

--- rust/kernel/miscdevice.rs
+++ rust/kernel/miscdevice.rs
@@ -19,7 +19,7 @@ use crate::{
     seq_file::SeqFile,
     types::{ForeignOwnable, Opaque},
 };
-use core::{marker::PhantomData, mem::MaybeUninit, pin::Pin};
+use core::{marker::PhantomData, pin::Pin};
 
 /// Options for creating a misc device.
 #[derive(Copy, Clone)]
@@ -31,8 +31,7 @@ pub struct MiscDeviceOptions {
 impl MiscDeviceOptions {
     /// Create a raw `struct miscdev` ready for registration.
     pub const fn into_raw<T: MiscDevice>(self) -> bindings::miscdevice {
-        // SAFETY: All zeros is valid for this C type.
-        let mut result: bindings::miscdevice = unsafe { MaybeUninit::zeroed().assume_init() };
+        let mut result: bindings::miscdevice = pin_init::zeroed();
         result.minor = bindings::MISC_DYNAMIC_MINOR as ffi::c_int;
         result.name = self.name.as_char_ptr();
         result.fops = MiscdeviceVTable::<T>::build();
@@ -359,8 +358,7 @@ impl<T: MiscDevice> MiscdeviceVTable<T> {
         } else {
             None
         },
-        // SAFETY: All zeros is a valid value for `bindings::file_operations`.
-        ..unsafe { MaybeUninit::zeroed().assume_init() }
+        ..pin_init::zeroed()
     };
 
     const fn build() -> &'static bindings::file_operations {
